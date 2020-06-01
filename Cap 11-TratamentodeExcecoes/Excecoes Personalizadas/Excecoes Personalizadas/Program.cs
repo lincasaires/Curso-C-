85cc6e7319a59a108599b505c7c483257b21c06a
Lucas Aires <lucasairesgodinho1@gmail.com>
2020-05-31 23:05:36 -0300

    Capitulo 11 adicionado

---
+++ Cap 11-TratamentodeExcecoes/Excecoes Personalizadas/Excecoes Personalizadas/Program.cs
@@ -0,0 +1,47 @@
+﻿using Excecoes_Personalizadas.Entities;
+using System;
+using Excecoes_Personalizadas.Entities.Exceptions;
+
+namespace Excecoes_Personalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date: (dd/MM/yyyy): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date: (dd/MM/yyyy): ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+                Reservation reservation = new Reservation(roomNumber, checkin, checkout);
+                Console.WriteLine(reservation);
+                Console.WriteLine();
+
+                Console.WriteLine("Enter data to update reservation:");
+                Console.Write("Check-in date: (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date: (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+                reservation.UpdateDates(checkin, checkout);
+                Console.WriteLine(reservation);
+            }
+
+            catch(DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
+        }
+    }
+}
---
+++ Cap 11-TratamentodeExcecoes/Try-Catch/Try-Catch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Division for 0 is not allowed! " + e.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Only int numbers are allowed! ");
+            }
+        }
+    }
+}
